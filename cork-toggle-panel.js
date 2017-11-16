12c997d9f5b9436dce742af9c9194cbf02c35d26
Jusitn Merz <jrmerz@gmail.com>
2017-11-16 11:03:53 -0800

    js module version

---
+++ cork-toggle-panel.js
@@ -0,0 +1,47 @@
+import {Element as PolymerElement} from "@polymer/polymer/polymer-element"
+import "@polymer/iron-icon/iron-icon"
+import "@polymer/iron-icons/iron-icons"
+
+import template from "./cork-toggle-panel.html"
+
+export class CorkTogglePanel extends PolymerElement {
+
+  static get properties() {
+    return {
+      opened: {
+        type: Boolean,
+        value: false
+      },
+      label: {
+        type : String,
+        value : ''
+      }
+    };
+  }
+
+  static get template() {
+    return template;
+  }
+
+  open() {
+    var h = 5;
+    for( var i = 0; i < this.children.length; i++ ) {
+      h += this.children[i].offsetHeight;
+    }
+    this.$.content.style.height = h+'px';
+    this.opened = true;
+  }
+
+  close() {
+    this.$.content.style.height = '0px';
+    this.opened = false;
+  }
+
+  toggle() {
+    if( this.opened ) this.close();
+    else this.open();
+  }
+
+}
+
+customElements.define('cork-toggle-panel', CorkTogglePanel);
